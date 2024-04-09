fc2957363c05e40b031e5031912948e9bae9c276
Antoinedelongeaux <34346098+Antoinedelongeaux@users.noreply.github.com>
2024-04-09 08:51:02 +0200

    v0.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,16 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Configurer l'objet 'node' pour désactiver les polyfills inutiles
+  // Ajouter un fallback pour 'crypto'
+  if (!config.resolve.fallback) {
+    config.resolve.fallback = {};
+  }
+  config.resolve.fallback.crypto = require.resolve('crypto-browserify');
+  config.resolve.fallback.stream = require.resolve('stream-browserify');
+  config.resolve.fallback.vm = require.resolve('vm-browserify');
+
+
+  // Configuration 'node' existante
   config.node = {
     global: false,
     __filename: false,
