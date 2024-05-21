e97963dc47edc4f4d16c868ea9b8e9bb2f815abc
Antoine <antoine@longeaux.com>
2024-05-21 09:30:55 +0200

    v0.0.7

--- src/components/sound_handling.js
+++ src/components/sound_handling.js
@@ -1,290 +1,3 @@
-{/* 
-import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
-import * as FileSystem from 'expo-file-system';
-import { supabase } from '../lib/supabase';
-import { Buffer } from 'buffer';
-import { decode } from 'base64-arraybuffer'
-import { Recording, RecordingOptionsPresets } from 'expo-av';
-
-global.Buffer = Buffer; 
-
-
-import { Platform } from 'react-native';
-
-export const save_audio = async (audioFileUri, name) => {
-  try {
-    console.log("audioFileUri : ", audioFileUri);
-
-    if (Platform.OS !== 'web') {
-      const fileInfo = await FileSystem.getInfoAsync(audioFileUri);
-      console.log("Taille du fichier avant upload: ", fileInfo.size);
-
-      const base64 = await FileSystem.readAsStringAsync(audioFileUri, { encoding: FileSystem.EncodingType.Base64 });
-      const fileName = name;
-
-      const { error: uploadError } = await supabase.storage.from('audio').upload(fileName, decode(base64), {
-        contentType: 'audio/mp3',
-        cacheControl: '3600',
-        upsert: false,
-      });
-
-      if (uploadError) {
-        throw uploadError;
-      }
-
-      const publicURL = "https://zaqqkwecwflyviqgmzzj.supabase.co/storage/v1/object/public/audio/" + fileName;
-      console.log(`File uploaded and accessible at: ${publicURL}`);
-      return publicURL;
-    } else {
-      console.log("Handling audio upload on web...");
-
-      // Vous devez avoir accès au Blob ou au File de l'enregistrement ici. 
-      // Par exemple, si `audioFileUri` est un objet Blob ou File directement :
-      const fileName = name;
-
-      // Note: Supabase accepte l'upload de Blob/File directement, sans conversion en base64
-      const { error: uploadError, data } = await supabase.storage.from('audio').upload(fileName, audioFileUri, {
-        contentType: 'audio/mp3',
-        cacheControl: '3600',
-        upsert: false,
-      });
-
-      if (uploadError) {
-        throw uploadError;
-      }
-
-      const publicURL = `https://zaqqkwecwflyviqgmzzj.supabase.co/storage/v1/object/public/audio/${fileName}`;
-      console.log(`File uploaded and accessible at: ${publicURL}`);
-      return publicURL;
-    }
-  } catch (error) {
-    console.error('Error in save_audio:', error);
-    return null;
-  }
-};
-
-
-export const delete_audio = async (name) => {
-  const { data, error } = await supabase
-    .storage
-    .from('audio')
-    .remove([name])
-
-}
-
-
-export const audioSetup = async () => {
-  await Audio.setAudioModeAsync({
-    allowsRecordingIOS: true,
-    playsInSilentModeIOS: true,
-    interruptionModeIOS: InterruptionModeIOS.DoNotMix,
-    shouldDuckAndroid: true,
-    interruptionModeAndroid: InterruptionModeAndroid.DoNotMix,
-    playThroughEarpieceAndroid: false,
-  });
-};
-
-export const requestPermissionsAsync = async () => {
-  const response = await Audio.requestPermissionsAsync();
-  return response.status === 'granted';
-};
-
-export const startRecording = async () => {
-  try {
-    const permissionGranted = await requestPermissionsAsync();
-    if (!permissionGranted) {
-      console.log('Permission to record audio was denied');
-      return;
-    }
-
-    await audioSetup();
-    const recording = new Audio.Recording();
-    await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
-    await recording.startAsync();
-    return recording;
-  } catch (error) {
-    console.error('Error starting recording', error);
-  }
-};
-
-export const stopRecording = async (recording, name) => {
-  try {
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI(); // Get the recorded file URI
-    console.log("Recording stopped. File saved at: ", uri);
-
-
-    // Directly pass the file URI to save_audio
-    const publicURL = await save_audio(uri, name);
-    console.log(`File uploaded and accessible at: ${publicURL}`);
-    return uri;
-  } catch (error) {
-    console.error('Error stopping recording or uploading:', error);
-  }
-};
-
-
-export const playRecording_fromURI = async (uri) => {
-  try {
-
-    const { sound } = await Audio.Sound.createAsync(
-      { uri },
-      { shouldPlay: true }
-    );
-    // Playback started
-    sound.setOnPlaybackStatusUpdate((playbackStatus) => {
-      if (!playbackStatus.isLoaded) {
-        // Error handling
-        console.log(playbackStatus.error);
-      } else {
-        if (playbackStatus.didJustFinish && !playbackStatus.isLooping) {
-          sound.unloadAsync(); // Unload the sound from memory when playback finishes
-        }
-      }
-    });
-  } catch (error) {
-    console.error('Error playing recording', error);
-  }
-};
-
-export const playRecording_fromAudioFile = async (publicURL, fileName) => {
-  try {
-    // Construire l'URL du fichier sur Supabase
-    console.log("publicURL : ", publicURL)
-    console.log("fileName : ", fileName)
-
-    // Télécharger le fichier audio dans le système de fichiers local
-    const uri = FileSystem.cacheDirectory + fileName;
-    const downloadResponse = await FileSystem.downloadAsync(publicURL, uri);
-
-    if (downloadResponse.status !== 200) {
-      throw new Error('Failed to download file');
-    }
-
-    // Lire le fichier audio téléchargé
-    const { sound } = await Audio.Sound.createAsync(
-      { uri: downloadResponse.uri },
-      { shouldPlay: true }
-    );
-
-    sound.setOnPlaybackStatusUpdate((playbackStatus) => {
-      if (!playbackStatus.isLoaded) {
-        // Gestion des erreurs
-        console.log(playbackStatus.error);
-      } else {
-        if (playbackStatus.didJustFinish && !playbackStatus.isLooping) {
-          sound.unloadAsync(); // Décharger le son de la mémoire une fois la lecture terminée
-        }
-      }
-    });
-  } catch (error) {
-    console.error('Error playing recording from Supabase file', error);
-  }
-};
-
-
-
-
-
-const downloadFileToLocalFileSystem = async (fileUri) => {
-  const localUri = FileSystem.documentDirectory + 'tempAudio.mp3';
-  const { uri } = await FileSystem.downloadAsync(fileUri, localUri);
-  return uri;
-};
-
-
-
-
-
-export const transcribeAudio = async (audioFileName) => {
-  try {
-    const publicURL = "https://zaqqkwecwflyviqgmzzj.supabase.co/storage/v1/object/public/audio/" + audioFileName;
-
-
-    // Création de l'objet FormData
-    const formData = new FormData();
-    formData.append('audio', {
-      uri: publicURL,
-      type: 'audio/mp3', // Google Cloud Speech-to-Text peut gérer d'autres types, mais il est préférable de convertir à LINEAR16 si nécessaire.
-      name: audioFileName,
-    });
-
-    const testUrl = 'https://91.108.112.18:3000/test';
-    const testResponse = await fetch(testUrl);
-    const testResult = await testResponse.text();
-    console.log('Test API response:', testResult);
-
-    const serverUrl = 'https://91.108.112.18:3000/transcript';
-    const response = await fetch(serverUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData,
-    });
-
-    if (!response.ok) {
-      const errorResponse = await response.text();
-      console.error('Error during transcription:', errorResponse);
-      throw new Error(`Server responded with an error: ${errorResponse}`);
-    }
-
-    // Traitement de la réponse
-    const transcriptionResult = await response.json();
-    console.log('Transcription:', transcriptionResult.transcription);
-    return transcriptionResult.transcription;
-  } catch (error) {
-    console.error('Error in transcribeAudio:', error.message);
-    return null;
-  }
-};
-
-
-// Assuming startRecording, stopRecording, and playRecording are imported or defined above
-export const record_answer = async (name) => {
-
-
-  const recording = await startRecording();
-
-  await new Promise(resolve => setTimeout(resolve, 3000));
-
-  const uri = await stopRecording(recording, name);
-  console.log(`Recording stopped. File saved at: ${uri}`);
-
-
-
-};
-
-const blobToBase64 = (blob) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-    reader.readAsDataURL(blob);
-  });
-};
-
-const saveBlobToFileSystem = async (blob, fileName) => {
-  const base64DataUrl = await blobToBase64(blob); // Cela retourne une Data URL
-  const base64ContentArray = base64DataUrl.split(","); // Séparer l'en-tête du contenu proprement dit
-  if (base64ContentArray.length !== 2) {
-    throw new Error('Base64 content is not in the expected format');
-  }
-  const base64 = base64ContentArray[1]; // Obtenez la partie base64 sans l'en-tête
-
-  const uri = FileSystem.documentDirectory + fileName;
-  await FileSystem.writeAsStringAsync(uri, base64, { encoding: FileSystem.EncodingType.Base64 });
-  return uri;
-};
-
-
-
-*/}
-
-
-
 import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av';
 import * as FileSystem from 'expo-file-system';
 import { supabase } from '../lib/supabase';
@@ -485,13 +198,20 @@ export const playRecording_fromURI = async (uri) => {
 
 export const playRecording_fromAudioFile = async (publicURL, fileName) => {
   try {
+    // Utilisation de la nouvelle URL de Supabase pour le fichier audio
+    publicURL = `https://zaqqkwecwflyviqgmzzj.supabase.co/storage/v1/object/public/audio/${publicURL}`;
+
+    // Log pour afficher l'URL et le nom du fichier audio
+    console.log("Playing audio from URL:", publicURL);
+    console.log("File name for non-web platforms:", fileName);
+
     // Sur les plateformes Web, créer un URL Object et le jouer
     if (Platform.OS === 'web') {
       const { sound } = await Audio.Sound.createAsync(
         { uri: publicURL },
         { shouldPlay: true }
       );
-  
+
       sound.setOnPlaybackStatusUpdate((playbackStatus) => {
         if (!playbackStatus.isLoaded) {
           console.error("Playback status: ", playbackStatus.error);
@@ -529,6 +249,7 @@ export const playRecording_fromAudioFile = async (publicURL, fileName) => {
     console.error('Error playing recording from Supabase file', error);
   }
 };
+
 
 
 
