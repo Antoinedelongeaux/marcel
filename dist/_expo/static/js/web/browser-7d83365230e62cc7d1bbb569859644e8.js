c07049a6d1bc25e0374decd7cdbd241dbb7af06c
Antoinedelongeaux <34346098+Antoinedelongeaux@users.noreply.github.com>
2024-07-15 12:20:31 +0000

    v1.0.12

--- dist/_expo/static/js/web/browser-7d83365230e62cc7d1bbb569859644e8.js
+++ dist/_expo/static/js/web/browser-7d83365230e62cc7d1bbb569859644e8.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 __d((function(g,r,i,a,m,e,d){'use strict';m.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}),1376,[]);
-=======
-__d((function(g,r,i,a,m,e,d){'use strict';m.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}),568,[]);
->>>>>>> cc2665d (v1.0.11)
