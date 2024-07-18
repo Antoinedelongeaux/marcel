b51296010e9d253e599447173e7253d6edad1abe
Antoine de Longeaux <antoine@longeaux.com>
2024-07-18 15:26:27 +0000

    v1.0.13

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,14 +1,15 @@
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
-const supabaseUrl = 'https://zaqqkwecwflyviqgmzzj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphcXFrd2Vjd2ZseXZpcWdtenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczNzg5NTYsImV4cCI6MjAyMjk1NDk1Nn0.R9AGSk_sS4oStZ3YSj1DjXUPja2Lb0vtF2jP0xQzq-4';
+//const supabaseUrl = 'https://zaqqkwecwflyviqgmzzj.supabase.co'
+//const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphcXFrd2Vjd2ZseXZpcWdtenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDczNzg5NTYsImV4cCI6MjAyMjk1NDk1Nn0.R9AGSk_sS4oStZ3YSj1DjXUPja2Lb0vtF2jP0xQzq-4';
 // const supabaseUrl = PUBLIC_SUPABASE_URL
 // const supabaseAnonKey = PUBLIC_SUPABASE_ANON_KEY
 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
